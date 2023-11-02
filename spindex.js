bbde7ddd82da385b9b713653f8cb1615da3b038d
luluxiaoyu <ljy.xiaoyu@qq.com>
2023-11-02 17:53:43 +0800

    2

---
+++ spindex.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.get('https://txtserver.vercel.app/?url=2');
+    const urls = response.data.split('\n'); // 将返回的多行url分割成数组
+
+    const randomIndex = Math.floor(Math.random() * urls.length);
+    const randomUrl = urls[randomIndex];
+
+    const imageResponse = await axios.get(randomUrl, { responseType: 'stream' });
+    res.set('Content-Type', imageResponse.headers['content-type']);
+    imageResponse.data.pipe(res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+app.listen(32000, () => {
+  console.log('Server is running on http://localhost:32000');
+});
