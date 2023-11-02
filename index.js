7f118db5a6486f908914af631a01216c32f9ca84
luluxiaoyu <ljy.xiaoyu@qq.com>
2023-11-02 17:41:53 +0800

    1

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
-const express = require('express');  
-const axios = require('axios');  
-const fs = require('fs');  
-const app = express();  
-const port = 32000;  
-app.use(express.static("assets")); 
-app.get('/', async (req, res) => {  
-  try {  
-    // 读取normal.txt文件中的所有行  
-    const fileContent = fs.readFileSync('./normal.txt', 'utf8');  
-    const lines = fileContent.split('\n');  
-  
-    // 随机选择一行作为图片URL  
-    const randomLineIndex = Math.floor(Math.random() * lines.length);  
-    const imageUrl = lines[randomLineIndex];  
-  
-    // 使用axios获取图片内容  
-    const response = await axios({  
-      method: 'get',  
-      url: imageUrl,  
-      responseType: 'stream',  
-    });  
-  
-    // 将图片内容写入响应中  
-    res.set('Content-Type', 'image/jpeg');  
-    response.data.pipe(res);  
-  } catch (error) {  
-    console.error(error);  
-    res.status(500).send('Internal Server Error');  
-  }  
-});  
-  
-app.listen(port, () => {  
-  console.log(`Server is running on port ${port}`);  
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.get('https://txtserver.vercel.app/api?url=1');
+    const urls = response.data.split('\n'); // 将返回的多行url分割成数组
+
+    const randomIndex = Math.floor(Math.random() * urls.length);
+    const randomUrl = urls[randomIndex];
+
+    const imageResponse = await axios.get(randomUrl, { responseType: 'stream' });
+    res.set('Content-Type', imageResponse.headers['content-type']);
+    imageResponse.data.pipe(res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/sp3jdciehf3q8rh8idfijARJ3F89R93RFJJFJ/', async (req, res) => {
+  try {
+    const response = await axios.get('https://txtserver.vercel.app/api?url=2');
+    const urls = response.data.split('\n'); // 将返回的多行url分割成数组
+
+    const randomIndex = Math.floor(Math.random() * urls.length);
+    const randomUrl = urls[randomIndex];
+
+    const imageResponse = await axios.get(randomUrl, { responseType: 'stream' });
+    res.set('Content-Type', imageResponse.headers['content-type']);
+    imageResponse.data.pipe(res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.listen(32000, () => {
+  console.log('Server is running on http://localhost:32000');
 });
